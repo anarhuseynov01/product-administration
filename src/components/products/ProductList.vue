a0375ffc8a20825d03bdcf5cee8f045e9a02f242
anarhuseynov <ahuseynov1995@gmail.com>
2019-10-13 01:41:58 +0400

    Componentlere ayirmaq

---
+++ src/components/products/ProductList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pt-3 mb-5 card mt-5 shadow">
+                <div class="card-body">
+                    <h3>Product List</h3>
+                    <hr>
+                    <table class="table table-hover table-striped table-bordered">
+                        <thead>
+                        <th>id</th>
+                        <th>Product Name</th>
+                        <th>Count</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td class="align-middle text-center"><span class="badge badge-info"> E564fghdE563df </span></td>
+                            <td class="align-middle text-center"> Test </td>
+                            <td class="align-middle text-center"> 1 </td>
+                            <td style="width: 120px;"> 10,000</td>
+                            <td class="align-middle"> Test Description</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="alert alert-warning" >
+                        <strong>We haven't found a record here yet</strong>
+                        <br>
+                        <small>You can use the Product Operations menu to add a record
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
