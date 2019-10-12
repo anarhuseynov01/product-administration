8f170654b33065cf4e878e36bc1fc7a7a0f44916
anarhuseynov <ahuseynov1995@gmail.com>
2019-10-13 02:38:37 +0400

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <app-header></app-header>
-    <app-container></app-container>
+    <router-view></router-view>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/shared/Header";
-import ProductPurchase from "./components/products/ProductPurchase";
-import ProductSell from "./components/products/ProductSell";
-import ProductList from "./components/products/ProductList";
+// import ProductPurchase from "./components/products/ProductPurchase";
+// import ProductSell from "./components/products/ProductSell";
+// import ProductList from "./components/products/ProductList";
 import Footer from "./components/shared/Footer";
 export default {
   name: 'app',
   components: {
     appHeader: Header,
-    appContainer: ProductList,
+    // appContainer: ProductPurchase,
     appFooter: Footer
   },
   data () {
--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -2,15 +2,15 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563F7A">
 
-        <a class="navbar-brand" href="#">Product List</a>
+        <router-link tag="a" class="nav-link" to="/product-list">Product List</router-link>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Product Operations</a>
+                    <router-link tag="a" class="nav-link" to="/product-entry" active-class="active">Product Operations</router-link>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Product Output</a>
+                    <router-link tag="a" class="nav-link" to="/product-sell" active-class="active">Product Output</router-link>
                 </li>
             </ul>
         </div>
