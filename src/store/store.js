9d6de8076d3537d29756b045b17ff0dfca9921a8
anarhuseynov <ahuseynov1995@gmail.com>
2019-10-20 01:22:07 +0400

    store operations

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,33 @@ import products from './modules/products';
 Vue.use(Vuex);
 
  export const store = new Vuex.Store({
-     state: {},
+     state: {
+         purchase: 0.0,
+         sale: 0.0,
+         balance: 0.0
+     },
+     getters: {
+         getTradeResult: state => {
+            return {
+                purchase: state.purchase,
+                sale: state.sale,
+                balance: state.balance
+            }
+         }
+     },
+     mutations: {
+        updateTradeResult(state,payload){
+
+        }
+     },
+     actions: {
+        setTradeResult({commit,state},payload){
+
+        },
+        getTradeResult({commit}){
+
+        }
+     },
      modules: {
          products
      }
