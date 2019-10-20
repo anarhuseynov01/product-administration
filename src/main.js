84b9de13eca0f79fc4375c9125a4e381417e788b
anarhuseynov <ahuseynov1995@gmail.com>
2019-10-20 23:08:45 +0400

    configuration vue-resource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { router } from './router'
-import { store } from './store/store'
+import Vue from 'vue';
+import App from './App.vue';
+import { router } from './router';
+import { store } from './store/store';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
 
 
 new Vue({
