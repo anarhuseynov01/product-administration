c536032b0f0ca891187202f3909d2a02dad48fbc
anarhuseynov <ahuseynov1995@gmail.com>
2019-11-02 15:56:22 +0400

    add currency filter

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -7,17 +7,17 @@
 
         <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
             <strong>Purchase Amount : </strong>
-            <span class="badge badge-danger">{{ getTradeResult.purchase }}</span>
+            <span class="badge badge-danger">{{ getTradeResult.purchase | currency}}</span>
         </span>
 
         <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
             <strong>Sales Amount : </strong>
-            <span class="badge badge-success">{{ getTradeResult.sale }}</span>
+            <span class="badge badge-success">{{ getTradeResult.sale | currency}}</span>
         </span>
 
         <span class="navbar-text navbar-nav my-2 my-lg-0">
             <strong>Gain : </strong>
-            <span class="badge badge-primary">{{ getTradeResult.balance }}</span>
+            <span class="badge badge-primary">{{ getTradeResult.balance | currency}}</span>
         </span>
 
     </nav>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
 
+Vue.filter("currency", (value) => {
+  return parseFloat(value).toLocaleString(undefined, {minimumFractionDigits: 2},) + " AZN"
+})
+
 
 new Vue({
   el: '#app',
